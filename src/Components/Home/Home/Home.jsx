58a509879f27e12caf589ad92344fc63985b8cf8
sojib-sudo <sojibmandol@gmail.com>
2023-02-18 23:33:48 +0600

    error solved

--- src/Components/Home/Home/Home.jsx
+++ src/Components/Home/Home/Home.jsx
@@ -14,14 +14,14 @@ const Home = () => {
             <Explore></Explore>
             <ReviewSlider></ReviewSlider>
             <BusinessSection></BusinessSection>
-<<<<<<< HEAD
+
 
             <FreelancerNationGuid></FreelancerNationGuid>
 
-=======
+
             <Guideline/>
             <TalentBanner/>
->>>>>>> 866377ad3d79d5f6d6639c55ea37ac6f0447d131
+
         </div>
     );
 };
