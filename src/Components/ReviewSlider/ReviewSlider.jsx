0e6be3d18e3aea9e7d46cc2a983909987016bc9b
sojib-sudo <sojibmandol@gmail.com>
2023-02-18 18:37:40 +0600

    Review Slider Component Added

---
+++ src/Components/ReviewSlider/ReviewSlider.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ReviewSlider = () => {
+    return (
+        <div className='font-roboto'>
+            <h2>Thsi is my Slider</h2>
+        </div>
+    );
+};
+
+export default ReviewSlider;
