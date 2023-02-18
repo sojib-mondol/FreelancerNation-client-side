adc233924fcc705d9bf856686ca655e215d64d01
Sayem92 <sayemmiha123@gmail.com>
2023-02-18 21:14:59 +0600

    add business section

---
+++ src/Components/buisnessSection/BuisnessSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BusinessSection = () => {
+    return (
+        <div>
+            This is a business section
+        </div>
+    );
+};
+
+export default BusinessSection;
