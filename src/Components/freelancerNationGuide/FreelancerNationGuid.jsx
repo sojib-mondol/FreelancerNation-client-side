e1d9fa9158a6f549c545990fe3ac9807f4126644
Sarwarhridoy4 <sarwarhridoy4@gmail.com>
2023-02-18 22:08:27 +0600

    Guig section added

---
+++ src/Components/freelancerNationGuide/FreelancerNationGuid.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FreelancerNationGuid = () => {
+    return (
+        <div>
+            <span>This is guid page</span>
+        </div>
+    );
+};
+
+export default FreelancerNationGuid;
