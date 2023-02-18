e74c49b57e7ad5af6d6bc238e793dafe70181984
Sheikh Riyadh <sheikhriyad350883@gmail.com>
2023-02-18 22:54:59 +0600

    created banner section

--- src/Components/Home/Home/Home.jsx
+++ src/Components/Home/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Banner from '../../Banner/Banner';
 import Explore from '../../Explore/Explore';
 import FeatureBanner from '../../FeatuteBanner/FeatureBanner';
 import ReviewSlider from '../../ReviewSlider/ReviewSlider';
@@ -6,6 +7,7 @@ import ReviewSlider from '../../ReviewSlider/ReviewSlider';
 const Home = () => {
     return (
         <div>
+            <Banner />
             <FeatureBanner></FeatureBanner>
             <Explore></Explore>
             <ReviewSlider></ReviewSlider>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,12 @@ module.exports = {
     },
     extend: {
       backgroundImage: {
-        'banner-1': "url('/src/assets/hero-banner-1.jpg')",
-        'banner-2': "url('/src/assets/hero-banner-2.jpg')",
-        'banner-3': "url('/src/assets/hero-banner-3.jpg')",
-        'banner-4': "url('/src/assets/hero-banner-4.jpg')",
-        'banner-5': "url('/src/assets/hero-banner-5.jpg')",
-        'banner-6': "url('/src/assets/hero-banner-6.jpg')",
+        'banner1': "url('/src/assets/hero-banner-1.jpg')",
+        'banner2': "url('/src/assets/hero-banner-2.jpg')",
+        'banner3': "url('/src/assets/hero-banner-3.jpg')",
+        'banner4': "url('/src/assets/hero-banner-4.jpg')",
+        'banner5': "url('/src/assets/hero-banner-5.jpg')",
+        'banner6': "url('/src/assets/hero-banner-6.jpg')",
       }
     },
   },
