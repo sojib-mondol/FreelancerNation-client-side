80d8d9fef9ef1a3dbf7b998d755a0601f2d337e1
Sheikh Riyadh <sheikhriyad350883@gmail.com>
2023-02-18 20:09:38 +0600

    install react icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,16 @@ module.exports = {
       montserrat: ["Montserrat", "sans-serif"],
       roboto: ["Roboto", "sans-serif"],
     },
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'banner-1': "url('/src/assets/hero-banner-1.jpg')",
+        'banner-2': "url('/src/assets/hero-banner-2.jpg')",
+        'banner-3': "url('/src/assets/hero-banner-3.jpg')",
+        'banner-4': "url('/src/assets/hero-banner-4.jpg')",
+        'banner-5': "url('/src/assets/hero-banner-5.jpg')",
+        'banner-6': "url('/src/assets/hero-banner-6.jpg')",
+      }
+    },
   },
   plugins: [],
 };
