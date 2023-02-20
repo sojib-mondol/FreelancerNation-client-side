36eafbaed08202e464efdeb4382d6315c3fe49f5
Sarwarhridoy4 <sarwarhridoy4@gmail.com>
2023-02-20 17:48:37 +0600

    home page content edited

--- src/Components/Home/Home/Home.jsx
+++ src/Components/Home/Home/Home.jsx
@@ -1,51 +1,28 @@
 import React from 'react';
+import Banner from '../../Banner/Banner'
+import ProfessionalServices from '../../ProfessionalServices/ProfessionalServices'
+import FeatureBanner from '../../FeatuteBanner/FeatureBanner'
+import Explore from '../../Explore/Explore'
+import ReviewSlider from '../../ReviewSlider/ReviewSlider'
+import BusinessSection from '../../buisnessSection/BuisnessSection'
+import Guideline from '../../Guideline/Guideline'
+import TalentBanner from '../../TalentBanner/TalentBanner'
 
-import Banner from '../../Banner/Banner';
-import BusinessSection from '../../buisnessSection/BuisnessSection';
-import Explore from '../../Explore/Explore';
-import FeatureBanner from '../../FeatuteBanner/FeatureBanner';
-import FreelancerNationGuid from '../../freelancerNationGuide/FreelancerNationGuid';
-import ProfessionalServices from '../../ProfessionalServices/ProfessionalServices';
-
-
-import Banner from '../../Banner/Banner';
-import Explore from '../../Explore/Explore';
-import FeatureBanner from '../../FeatuteBanner/FeatureBanner';
-import ProfessionalServices from '../../ProfessionalServices/ProfessionalServices';
-
-import BusinessSection from '../../buisnessSection/BuisnessSection';
-import Explore from '../../Explore/Explore';
-import FeatureBanner from '../../FeatuteBanner/FeatureBanner';
-import Guideline from '../../Guideline/Guideline';
-
-
-import ReviewSlider from '../../ReviewSlider/ReviewSlider';
-import TalentBanner from '../../TalentBanner/TalentBanner';
 
 
 const Home = () => {
     return (
         <div>
-            <Banner />
+            <Banner/>
             <ProfessionalServices />
             <FeatureBanner></FeatureBanner>
             <Explore></Explore>
             <ReviewSlider></ReviewSlider>
             <BusinessSection></BusinessSection>
-
-            <FreelancerNationGuid></FreelancerNationGuid>
-
-
             <Guideline />
             <TalentBanner />
-
-
-
             <Guideline/>
             <TalentBanner />
-            
-
-
         </div>
     );
 };
