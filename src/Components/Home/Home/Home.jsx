ab3c7f5ea12d280c59ca074ffc05b87e3bdc703d
Sheikh Riyadh <sheikhriyad350883@gmail.com>
2023-02-20 15:07:52 +0600

    chanage slider image size

--- src/Components/Home/Home/Home.jsx
+++ src/Components/Home/Home/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Banner from '../../Banner/Banner';
 import BusinessSection from '../../buisnessSection/BuisnessSection';
 import Explore from '../../Explore/Explore';
 import FeatureBanner from '../../FeatuteBanner/FeatureBanner';
 import FreelancerNationGuid from '../../freelancerNationGuide/FreelancerNationGuid';
+import ProfessionalServices from '../../ProfessionalServices/ProfessionalServices';
 import ReviewSlider from '../../ReviewSlider/ReviewSlider';
 import Guideline from '../Guideline/Guideline';
 import TalentBanner from '../TalentBanner/TalentBanner';
@@ -10,17 +12,17 @@ import TalentBanner from '../TalentBanner/TalentBanner';
 const Home = () => {
     return (
         <div>
+            <Banner />
+            <ProfessionalServices />
             <FeatureBanner></FeatureBanner>
             <Explore></Explore>
             <ReviewSlider></ReviewSlider>
             <BusinessSection></BusinessSection>
-
-
             <FreelancerNationGuid></FreelancerNationGuid>
 
 
-            <Guideline/>
-            <TalentBanner/>
+            <Guideline />
+            <TalentBanner />
 
         </div>
     );
