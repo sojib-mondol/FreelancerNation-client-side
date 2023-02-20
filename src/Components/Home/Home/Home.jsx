4552769b8c6f6e1a7321a74317c75ea4fab0a1aa
sojib-sudo <sojibmandol@gmail.com>
2023-02-20 15:06:05 +0600

    code updated

--- src/Components/Home/Home/Home.jsx
+++ src/Components/Home/Home/Home.jsx
@@ -19,7 +19,7 @@ const Home = () => {
             <FreelancerNationGuid></FreelancerNationGuid>
 
 
-            <Guideline/>
+            <Guideline/> 
             <TalentBanner/>
 
         </div>
--- src/Components/buisnessSection/BuisnessSection.jsx
+++ src/Components/buisnessSection/BuisnessSection.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const BusinessSection = () => {
     return (
         <div>
-            This is a business section
+            <h3>Get inspired with projects made by our freelancers</h3>
+            
         </div>
     );
 };
